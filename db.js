def90e6ceff55004e4eff6d23b4dad74a63dc4ca
rishabh-bourasi <41615024+rishabh-bourasi@users.noreply.github.com>
2025-09-20 17:17:01 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/ai-todo", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
